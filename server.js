dad5ed00c581f53fb5a83b38b8bd1066da8e9f0f
Julian Hoyos <jhoyos135@gmail.com>
2019-05-07 18:41:10 -0400

    changed app name

---
+++ server.js
@@ -0,0 +1,21 @@
+// npm scripts:
+// server: npm run server
+// client: npm run client
+// both:   npm run dev
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+// React Front-End 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  };
