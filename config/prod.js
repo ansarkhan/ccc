3f7b292eb99fdc88f999f162cd845183cae897d0
Julian Hoyos <jhoyos135@gmail.com>
2019-05-07 20:12:43 -0400

    switch config around

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    awsID: 'AKIAVYR463QKO4ES3N4J',
-    awsSECRET: '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'
+    awsID: process.env.AWS_ID,
+    awsSECRET: process.env.AWS_SECRET
 }
