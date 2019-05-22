12758932427881d567d61af88f3f9d41141d0604
Logan Shahan <loganshahan88@gmail.com>
2019-05-22 17:23:20 -0400

    sidenav

---
+++ client/src/components/SideNav/SideNav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./SideNav.css";
+
+
+const SideNav = () => (
+//   
+<div className="sidenav">
+<ul>
+  <li><a href="#albums">Albums</a></li>
+  <li><a href="#allpics">All Pictures</a></li>
+  <li><a href="#upload">Upload Pictures</a></li>
+  
+</ul>
+</div>
+
+
+);
+
+  export default SideNav;
