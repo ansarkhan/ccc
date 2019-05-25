7fea07a9dc57f23f2efa116fe976f95daa227f2e
ansarkhan <ansarkhan@me.com>
2019-05-25 10:52:32 -0400

    edit expense page skeleton

---
+++ client/src/components/Pictures/EditPicture.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Pictures.css';
+import axios from 'axios';
+import {Container, Col, Row} from 'react-bootstrap';
+
+
+const EditPicture = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditPicture
+
+
