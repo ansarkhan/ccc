c338982348b86b4907d8c2461d3f9af4eda877e9
Julian Hoyos <jhoyos135@gmail.com>
2019-05-31 18:32:25 -0400

    fixed config file

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB_URI: process.env.MONGODB_URI
-    ORIGIN: process.env.ORIGIN
+    mongoURI: process.env.MONGODB_URI
+    origin: process.env.ORIGIN
     // awsREGION: process.env.AWS_DEFAULT_REGION
 }
